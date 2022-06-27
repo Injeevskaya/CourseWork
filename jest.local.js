92d4bfff730b636a198d63de0f4a1c36624e49fa
TatianaInzhee <tatiana@fastforward.ai>
2022-06-27 19:37:22 +0200

    Add allure

--- jest.local.js
+++ jest.local.js
@@ -5,9 +5,18 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest',
   },
   testMatch: ['**/specs/*.spec.*'],
+  testRunner: 'jest-jasmine2',
+  setupFilesAfterEnv: ['jest-allure/dist/setup'],
   globals: {
     testTimeout: 50000,
   },
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      filename: 'report.html',
+      openReport: true,
+    }],
+  ],
   verbose: true,
 };
 
